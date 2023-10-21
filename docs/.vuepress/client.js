247d5c726ba1aae041261e1cfbf4bc62e09431fa
Ashley Solomon <ashcslmn@gmail.com>
2023-10-22 07:56:04 +0800

    remove custom layout

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,11 +1,7 @@
 import { defineClientConfig } from '@vuepress/client'
-import LayoutCategory from './layouts/Category.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {},
   rootComponents: [],
-  layouts: {
-    category: LayoutCategory
-  },
 })
