7fbad4934e0f736462c8406439c86159ec319ab4
dv_arrvs <lolhacker121@gmail.com>
2023-12-27 08:22:51 +0800

    Search: Algolia search not working #2 - update package.json dependencies for sitemap - search WIP

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,38 @@
-import { defineUserConfig, defaultTheme } from 'vuepress'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-import { navbar } from './config/navbar'
-import { sidebar } from './config/sidebar'
+import { defineUserConfig, defaultTheme } from "vuepress";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import { navbar } from "./config/navbar";
+import { sidebar } from "./config/sidebar";
+import { sitemapPlugin } from "vuepress-plugin-sitemap2";
 
 export default defineUserConfig({
-  lang: 'en-US',
-  title: 'Playbook',
-  description: "Outsoar\'s collection of strategies, best practices, guidelines, standards, recommendations, and conventions to provide better service.",
+  lang: "en-US",
+  title: "Playbook",
+  description:
+    "Outsoar's collection of strategies, best practices, guidelines, standards, recommendations, and conventions to provide better service.",
   theme: defaultTheme({
     navbar: navbar,
     sidebar: sidebar,
-    editLinkText: 'Edit on GitHub',
-    logo: 'images/logo.png',
-    repo: 'outsoarph/playbook',
-    docsRepo: 'https://github.com/outsoarph/playbook',
-    docsBranch: 'main',
-    docsDir: 'docs',
-    editLinkPattern: ':repo/edit/:branch/:path',
+    editLinkText: "Edit on GitHub",
+    logo: "images/logo.png",
+    repo: "outsoarph/playbook",
+    docsRepo: "https://github.com/outsoarph/playbook",
+    docsBranch: "main",
+    docsDir: "docs",
+    editLinkPattern: ":repo/edit/:branch/:path",
   }),
-  plugins: [ docsearchPlugin({
-    // apiKey: '773c0bdd07d5f8f011499c70ebf1b6fa',
-    apiKey: '91062141f601231fa4b77513931e3a3f',
-    indexName: 'playbook-outsoar',
-    appId: 'ZWU9Y2C1JS',
-    placeholder: 'Search',
-    searchParameters: {
-      facetFilters: ['language:en'],
-    },
-  })],
-})
+  plugins: [
+    docsearchPlugin({
+      // apiKey: '773c0bdd07d5f8f011499c70ebf1b6fa',
+      apiKey: "91062141f601231fa4b77513931e3a3f",
+      indexName: "playbook-outsoar",
+      appId: "ZWU9Y2C1JS",
+      placeholder: "Search",
+      searchParameters: {
+        facetFilters: ["language:en"],
+      },
+    }),
+    sitemapPlugin({
+      hostname: "https://playbook.outsoar.ph/",
+    }),
+  ],
+});
